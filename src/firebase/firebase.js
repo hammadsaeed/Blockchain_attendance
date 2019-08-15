999e7b16ae08bcaebb2abb1efd9dc079c5486187
hammadsaeed <35691714+hammadsaeed@users.noreply.github.com>
2019-08-15 13:07:19 +0800

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,21 +3,11 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const prodConfig = {
-  apiKey: "AIzaSyBEAQAz1U-LHBrTnwC1iirdHq4_dtj1NEc",
-  authDomain: "presence-unmc.firebaseapp.com",
-  databaseURL: "https://presence-unmc.firebaseio.com",
-  projectId:"presence-unmc",
-  storageBucket: 'presence-unmc.appspot.com',
-  messagingSenderId: "194613385208"
+  
 };
 
 const devConfig = {
-    apiKey: "AIzaSyBEAQAz1U-LHBrTnwC1iirdHq4_dtj1NEc",
-    authDomain: "presence-unmc.firebaseapp.com",
-    databaseURL: "https://presence-unmc.firebaseio.com",
-    projectId:"presence-unmc",
-    storageBucket: 'presence-unmc.appspot.com',
-    messagingSenderId: "194613385208"
+
 };
 
 const config = process.env.NODE_ENV === 'production'
